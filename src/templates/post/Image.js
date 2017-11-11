aabd523c31a54f1f810cca656392e421a95f6f96
Malte Modrow <modrow@ubilabs.net>
2017-11-11 13:45:24 +0100

    fix images

--- src/templates/post/Image.js
+++ src/templates/post/Image.js
@@ -13,9 +13,9 @@ export default styled(Img)`
   img {
     width: 100%;
     height: 100%;
-    object-fit: cover;
-    object-position: center;
     vertical-align: middle;
     position: absolute;
+    object-fit: initial; !important
+    object-position: initial; !important
   }
 `;
